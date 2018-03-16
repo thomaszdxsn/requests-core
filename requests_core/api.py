d0392050ef7b2d6cc23baa07a338403efc5daf9c
Kenneth Reitz <me@kennethreitz.org>
2018-03-16 08:12:38 -0400

    accept kwargs
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- requests_core/api.py
+++ requests_core/api.py
@@ -3,13 +3,13 @@ import trio
 from .http_manager import AsyncPoolManager
 from .http_manager._backends import TrioBackend
 
-async def request(method, url, pool=None, preload_content=False):
+async def request(method, url, pool=None, preload_content=False, **kwargs):
     if not pool:
         pool = AsyncPoolManager(backend=TrioBackend())
 
     with pool as http:
-        r = await http.request(method.upper(), url, preload_content=preload_content)
+        r = await http.request(method.upper(), url, preload_content=preload_content, **kwargs)
         return r
 
-def blocking_request(method, url, *, pool=None, preload_content=False):
+def blocking_request(method, url, *, pool=None, preload_content=False, **kwargs):
     return trio.run(request, method, url, pool, preload_content)
