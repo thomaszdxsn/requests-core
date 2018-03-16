12507972f0466ec4d8f4662f55f0385ba496bf9d
Kenneth Reitz <me@kennethreitz.org>
2018-03-16 08:55:15 -0400

    updates
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- requests_core/api.py
+++ requests_core/api.py
@@ -3,15 +3,54 @@ import trio
 from .http_manager import AsyncPoolManager
 from .http_manager._backends import TrioBackend
 
-async def request(method, url, pool=None, preload_content=False, **kwargs):
+
+async def request(
+    method,
+    url,
+    body=None,
+    headers=None,
+    # encode_multipart=True,
+    # multipart_boundary=None,
+    preload_content=False,
+    pool=None,
+    **kwargs
+):
     """Returns a Response object, to be awaited."""
     if not pool:
         pool = AsyncPoolManager(backend=TrioBackend())
-
     with pool as http:
-        r = await http.request(method.upper(), url, preload_content=preload_content, **kwargs)
+        r = await http.request(
+            method=method,
+            url=url,
+            headers=headers,
+            # encode_multipart=encode_multipart,
+            # multipart_boundary=multipart_boundary,
+            preload_content=preload_content,
+            **kwargs
+        )
         return r
 
-def blocking_request(method, url, *, pool=None, preload_content=False, **kwargs):
+
+def blocking_request(
+    method,
+    url,
+    body=None,
+    headers=None,
+    # encode_multipart=True,
+    # multipart_boundary=None,
+    preload_content=False,
+    pool=None,
+    # **kwargs
+):
     """Returns a Response object."""
-    return trio.run(request, method, url, pool, preload_content)
+    return trio.run(
+        request,
+        method,
+        url,
+        body,
+        headers,
+        # encode_multipart,
+        # multipart_boundary,
+        preload_content,
+        pool
+    )
--- t.py
+++ t.py
@@ -3,13 +3,15 @@ import requests_core
 
 URL = "http://httpbin.org/uuid"
 
+
 async def main():
     r = await requests_core.request('GET', URL)
     print(r.headers)
     print(await r.read())
 
+
 if __name__ == '__main__':
     import trio
-    trio.run(main)
 
+    trio.run(main)
 print(requests_core.blocking_request('GET', URL))
